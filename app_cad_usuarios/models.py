49a287409b043fa159a681188a78770208cc23e3
MarcioVFM <marciov859@gmail.com>
2024-08-14 12:14:00 -0400

    Projeto finalizado

---
+++ app_cad_usuarios/models.py
@@ -0,0 +1,7 @@
+#Pagina para criacao de class do banco de dados
+from django.db import models
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    idade = models.IntegerField()
